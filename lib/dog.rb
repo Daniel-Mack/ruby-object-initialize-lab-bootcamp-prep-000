8bcbae338a20b2f05228b74c0262ce0335d31565
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-22 13:09:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,18 @@ class Dog
   @name = name
   @breed = name
  end
- def name=(name)
+ def name_breed=(name, breed)
    @name = name
+   @breed = breed
  end
- def name
+ def name_breed
    @name
+   @breed
  end
 end
 
 fido = Dog.new
 fido.name = "Fido"
 puts fido.name
+fido.breed = "Beagle"
+puts fido.breed
